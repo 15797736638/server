58f12503edfef37c8e54be88b91cf981e91e32ef
huang <m15797736638@163.com>
2021-07-10 09:14:42 +0800

    first commit

---
+++ programs/OS/thread/Thread/Thread.cpp
@@ -0,0 +1,90 @@
+#include "Thread.h"
+
+#ifdef WIN32
+DWORD WINAPI CThread::_ThreadEntry(LPVOID pParam)
+#else
+void* CThread::_ThreadEntry(void* pParam)
+#endif
+{
+    CThread *pThread = (CThread *)pParam;
+    if(pThread->InitInstance()){
+        pThread->Run();
+    }
+    pThread->ExitInstance();
+    return NULL;
+}
+
+CThread::CThread(){
+    m_hThread = (THREAD_HANDLE)0;
+    m_IDThread = 0;
+}
+
+CThread::~CThread(){}
+
+bool CThread::Create(){
+    if(m_hThread != (THREAD_HANDLE)0){
+        return true;
+    }
+    bool ret = true;
+#ifdef WIN32
+    m_hThread = ::CreateThread(NULL,9,_ThreadEntry,this,0,&m_IDThread);
+    if(m_hThread == NULL){
+        ret = false;
+    }
+#else
+    ret = (::pthread_create(&m_hThread,NULLm&_ThreadEntry,this) == 0);
+#endif
+    return ret;
+}
+
+bool CThread::InitInstance(){
+    return true;
+}
+
+void CThread::ExitInstance(){}
+
+void CThread::OSSleep(int seconds){
+#ifdef WIN32
+    ::Sleep(seconds*1000);
+#else
+    ::Sleep(seconds);
+#endif
+}
+
+void CThread::SleepMs(int nMilliseconds){
+#ifdef Win32
+    ::Sleep(nMilliseconds);
+#else
+    ::usleep(nMilliseconds);
+#endif
+}
+
+bool CThread::IseCurrentThread(){
+#ifdef WIN32
+    return ::GetCurrentThreadID() == m_IDThread;
+#else 
+    return ::pthread_self() == m_hThread;
+#endif
+}
+
+bool CThread::Join(){
+    THREAD_HANDLE m_hThread = GetHandle();
+    if(m_hThread == (THREAD_HANDLE)0){
+        return true;
+    }
+#ifdef WIN32
+    return (::WaitForSingleObject(hThread,INFINITE) != 0);
+#else
+    return (::pthread_join(hTread,NULL) == 0);
+#endif
+}
+
+void CThread::ExitThread(){
+#ifdef WIN32
+    ::ExitThread(0);
+#else 
+#endif
+
+}
+
+
